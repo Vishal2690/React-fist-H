e3b6f44670c3681cf8833e2f2261a0dbeb04aed2
Vishal2690 <vishalc2690@gmail.com>
2023-03-23 19:23:53 +0530

    created app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// First HandSon
 
 import React from "react";
 import {useState} from "react";
@@ -14,15 +15,112 @@ const App=()=>{
     const clickbtn1 = () => {
       setflag(!flag)
     }
-    
-      
-  return (<div> 
+
+  return (<div>
           <div className="Give-col"><h1>Styling using Function and Class Component</h1> </div>
           <button className="Btn1" onClick={clickbtn}>To see styling in functional component</button>
           <button className="Btn2" onClick={clickbtn1}>To see styling in class component</button>
           {show ? <FunctionComponent/>:" "}
-          {flag ? <ClassComponent/>:" "}
+          {flag ? <ClassComponent/> : " "}
     </div>
   )
+
 }
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//***********************************************************************************************************
+
+
+// Second HandSon
+
+// import React, { Component } from "react";
+// import "./Component/SecondHandson/Style.css";
+
+// export default class App extends Component {
+//   state = {
+
+//     Name2 : "",
+//     Department : "",
+//     Rating : "",
+//     employees : [],
+//   };
+
+//   changeHandler = (v) => {
+//     this.setState({ [v.target.name]: v.target.value });
+//   };
+
+//   formSubmit = (v) => {
+//     const empObj = 
+//     {
+//       Name: this.state.Name2,
+//       Department: this.state.Department,
+//       Rating: this.state.Rating,
+//     };
+//     const arr = this.state.employees;
+//     arr.push(empObj);
+//     this.setState({ employees: arr });
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <h1>EMPLOYEE FEEDBACK FORM</h1>
+//         <div className="listOfAdding">
+//           <form>
+//             <label htmlFor="Name" id="size">Name : </label>
+//             <input   id="moving"  type="text" name="Name2" value={this.state.Name}  onChange={this.changeHandler}></input>
+//             <br />
+//             <label>Department : </label>
+//             <input    id="moving2"   type="text"   name="Department"  value={this.state.Department}   onChange={this.changeHandler}></input>
+//             <br />
+//             <label id="size2">Rating : </label>
+//             <input   id="moving3" type="number"  name="Rating"  value={this.state.Rating} onChange={this.changeHandler}></input>
+//             <br />
+//           </form>
+//         </div>
+//         <div className="addBtn">
+//           <button type="button" onClick={this.formSubmit}>
+//             Submit
+//           </button>
+//         </div>
+
+//         <div  className="hideCssAdd">
+//           {this.state.employees.map((value,index) => 
+//           {
+//             return (
+          
+//                 <span className="hideCssAd" ket={index}>{value.Name}{value.Department}{value.Rating}</span>
+              
+//             );
+//           })}
+//         </div>
+//       </>
+//     );
+//   }
+// }
+
+
+
+
--- src/Component/FirstHandson/ClassComponent.jsx
+++ src/Component/FirstHandson/ClassComponent.jsx
@@ -3,15 +3,24 @@ import './FristStyle.css'
 import { Component } from 'react';
 
  class ClassComponent extends Component{
+   // constructor(){
+   //    super();
+   //    this.state={
+   //       clickbtn1:false
+   //    }
+   // }
    render(){
       return (
-         
+         <>
+           {/* <button className="Btn2" onClick={()=>this.setState({clickbtn1: !this.state.clickbtn1})}>To see styling in class component</button> */}
+          {/* {this.state.clickbtn1 ?<ClassComponent/>:null} */}
          <div className='Chagecolor'>
-            <h1>This is created using finctional component</h1>
+            <h1>This is created using Class component</h1>
             <h2>This is done using external css</h2>
             <h4>This is done using inline css</h4>
         </div>
-         
+        
+        </>
       )
    }
  }
